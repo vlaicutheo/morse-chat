eb69c7fb8f206ab9c5c4109aa6998f5451a66a77
vlaicutheo <95164828+vlaicutheo@users.noreply.github.com>
2022-04-02 16:23:42 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+})
+basic.forever(function () {
+	
+})
